9d450d403da8e9e368914aff717b51e6f7fdf5e3
Alex <amhughes@g.cofc.edu>
2016-02-08 13:14:44 -0500

    completed functionality

---
+++ src/Inventory.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Created by alexanderhughes on 2/8/16.
+ */
+public class Inventory {
+
+    public static ArrayList<ItemMg> items = new ArrayList<>();
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while (true) {
+
+            int i = 1;
+            for (ItemMg item : items) {
+                System.out.println(i + " [" + item.quantity + "]" + ". " + item.name);
+                i++;
+            }
+            System.out.println();
+            System.out.println("1. Add new item.");
+            System.out.println("2. Remove item.");
+            System.out.println("3. Update item quantity");
+
+            String option = scanner.nextLine();
+
+            switch (option) {
+
+                case "1":
+                    System.out.print("Enter new item: ");
+                    String text = scanner.nextLine();
+                    items.add(new ItemMg(text, "0"));
+                    break;
+
+                case "2":
+                    System.out.print("Enter the index number of the item you want to remove: ");
+                    int itemNum = Integer.valueOf(scanner.nextLine());
+                    items.remove(itemNum - 1);
+                    break;
+
+                case "3":
+                    System.out.print("Enter the index number of the item you would like to increase: ");
+                    itemNum = Integer.valueOf(scanner.nextLine());
+                    ItemMg itemChoice = items.get(itemNum - 1);
+                    System.out.print("How much would you like to add to " + itemChoice.name + ": ");
+                    itemChoice.quantity = scanner.nextLine();
+                    break;
+
+                default:
+                    System.out.println("Invalid Response!!!");
+                    break;
+            }
+        }
+    }
+}
---
+++ src/ItemMg.java
@@ -0,0 +1,12 @@
+/**
+ * Created by alexanderhughes on 2/8/16.
+ */
+public class ItemMg {
+    String name;
+    String quantity;
+
+    public ItemMg(String name, String quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+}
